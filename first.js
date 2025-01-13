72baf395434802c4caa68fb6dabc6403bcdfa30a
RajJadhav28 <jadhavraj326@gmail.com>
2025-01-14 02:15:50 +0530

    JavaScript programs

---
+++ first.js
@@ -0,0 +1,44 @@
+console.log("Apna College");
+console.log("I Love JS");
+console.log("Hello World");
+console.log("Apna time ayega")
+age=24;
+console.log(age);
+
+console.log(age+1);
+name="Tonny Stark";
+console.log(name);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+isFollow=false;
+console.log(isFollow);
+doesFollow=true;
+console.log(doesFollow);
+name=25;
+console.log(name);
+
+//1>Variable names are case sensitive; a and A are different.
+fullname="Raj Jadhav";
+FullName="Jay Jadhav";
+console.log(fullname);
+console.log(FullName);
+// 2>Only letters, digits, underscore(_) and $ is allowed.(not even space)
+// full space="tony stark";-->not allowed--will throw an error
+fullName="Brain strom";
+fullName123="Denim Bravo";
+console.log(fullName);
+console.log(fullName123);
+// 3>Only a letter, underscore(_) or $ should be 1st character.
+// 123fullName="Max Tyres" not allowed.
+_fullName="Max tyres";
+console.log(_fullName);
+//4>Reserved wprds cannot be variable names.
+
+// Cases
+// fullName-->Camel case(this case is used.)
+// fullname-->XXX(will not be used)
+// full_name-->XXX(will not be used)--(Snake case)
+// full-name-->(Kabab case)
+// FullName-->Pascal Case
---
+++ second.js
@@ -0,0 +1,55 @@
+let fullName="tony stark brain";
+console.log(fullName);
+let age=24;
+let totalPrice=1000;
+
+console.log(age);
+const PI=3.14;
+console.log(PI);
+let a;
+console.log("A:"+a);
+a=10;
+console.log(a);
+{
+    let b=50;
+    console.log("First block:")
+    console.log("B:"+b);
+}
+{
+    let b=40;
+    console.log("Second block:");
+    console.log("B:"+b);
+}
+let age1=24;
+let price=100.50;
+typeof age1
+console.log(typeof age1);
+let full_name="Apna College";
+console.log(typeof full_name);
+let isFollow=true;
+console.log(typeof isFollow);
+let x;
+console.log(typeof x);//undefined will be the output.
+console.log(x);//undefined will be the output.
+let y=null;
+console.log(typeof y);//object will be output
+console.log(y);//null will be output
+let z=null;
+console.log(z);//null will be output
+console.log(typeof z);//object will be output
+let w=BigInt("123");
+console.log(w);
+console.log(typeof w);
+let w1=Symbol("Hello");
+console.log(w1);
+console.log(typeof w1);
+
+const student={
+    fullName:"Sonu Yadav",
+    age:20,
+    cgpa:802,
+    isPass:true
+};
+console.log(student);
+console.log(typeof student);
+console.log(student["fullName"]);
